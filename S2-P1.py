a6c0bd59a32e089dbff5fd1252b2141dd41d4fcf
AmandeepSingh789 <102435246+AmandeepSingh789@users.noreply.github.com>
2022-04-01 00:24:01 -0700

    Add files via upload

---
+++ S2-P1.py
@@ -0,0 +1,16 @@
+def summing(x,y):
+	print(x+y," ",type(x+y))
+
+def diff(x,y):
+	print(x-y," ",type(x-y))
+
+def product(x,y):
+	print(x*y," ",type(x*y))
+
+def main():
+	summing(4.7,6.9)
+	diff(78,53)
+	product(5.4,8)
+
+if __name__=="__main__":
+	main()
