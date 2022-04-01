9b3729a625e05c6bad4a16007106519c02f4b285
AmandeepSingh789 <102435246+AmandeepSingh789@users.noreply.github.com>
2022-04-01 00:23:25 -0700

    Add files via upload

---
+++ S1-P1.py
@@ -0,0 +1,12 @@
+def PrintDetails():
+	print("Amandeep Singh")
+	print("He/Him/His")
+	print("My Favorite Movie is Inside Out.")
+	print("I love the movie because of its potrayal of mental health in such a unique manner.")
+	print("My favorite food is burrito.")
+
+def main():
+	PrintDetails()
+
+if __name__=="__main__":
+	main()
